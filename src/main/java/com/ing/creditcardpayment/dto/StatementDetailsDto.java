6b5373f3fa208e9e98ae7414ae1dc9a6c372093b
lakshmi9354 <mplnarasimham@gmail.com>
2019-08-20 15:48:56 +0530

    stmt details

---
+++ src/main/java/com/ing/creditcardpayment/dto/StatementDetailsDto.java
@@ -0,0 +1,18 @@
+package com.ing.creditcardpayment.dto;
+
+import java.util.Date;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+@Getter @Setter @ToString @NoArgsConstructor
+public class StatementDetailsDto {
+	
+	private Long creditCardNumber;
+	private double amount;
+	private String transactionType;
+	private Date transactionDate;
+	private String reason;
+	}
+
